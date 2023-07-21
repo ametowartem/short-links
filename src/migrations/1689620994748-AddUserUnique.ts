808e74dc831b2c49eac5c4cfc8b45ff3143f9c02
ametowartem <215750@edu.fa.ru>
2023-07-21 03:31:29 +0300

    all project

---
+++ src/migrations/1689620994748-AddUserUnique.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddUserUnique1689620994748 implements MigrationInterface {
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'CREATE UNIQUE INDEX uq__user_entity__username on user_entity(username)',
+    );
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP UNIQUE INDEX uq__user_entity__username');
+  }
+}
