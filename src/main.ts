c18e60fe2ee6860c27433870628f659b64bbc00e
ametowartem <215750@edu.fa.ru>
2023-10-09 00:49:42 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`/docs`, app, document);
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT, '127.0.0.1');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
