e443c883fffa2679524ebf21e720bebda5539ee3
ametowartem <215750@edu.fa.ru>
2023-10-09 01:03:47 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import * as process from "process";
+import * as process from 'process';
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -18,7 +20,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`/docs`, app, document);
-
   await app.listen(process.env.PORT, '127.0.0.1');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
